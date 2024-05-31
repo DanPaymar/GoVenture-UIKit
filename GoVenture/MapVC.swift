099e316ccd91b2c698a6074c27cb3c5e88f47841
arcticchimpanzee <152743640+arcticchimpanzee@users.noreply.github.com>
2024-05-31 14:08:12 -0700

    Initial Commit

---
+++ GoVenture/MapVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GoVenture
+//
+//  Created by Daniel Paymar on 5/31/24.
+//
+
+import UIKit
+import MapBoxMaps
+
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
